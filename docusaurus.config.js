52f72344528fae51f8e7cd6810a6c75010fcffec
Lukas Merz <lukemerz@gmail.com>
2022-04-17 09:56:21 +0200

    Cleanup

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,8 +23,6 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: {
           path: 'meeting-notes',
