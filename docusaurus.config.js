fefbdb04e84bf127cd50e410bb5a8b9edf597350
Lukas Merz <lukemerz@gmail.com>
2022-05-08 13:51:53 +0200

    Add basic domain model

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,6 +23,7 @@ const config = {
             ({
                 docs: {
                     sidebarPath: require.resolve('./sidebars.js'),
+                    remarkPlugins: [require('mdx-mermaid')],
                 },
                 blog: {
                     path: 'meeting-notes',
